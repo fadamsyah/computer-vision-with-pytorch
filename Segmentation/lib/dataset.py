520529d8e10ab0a1a88dc8ab17eda653512e0ec9
adamsyah <fadillahadam11@gmail.com>
2021-03-11 17:47:20 +0700

    Add CVC-clinic-db dataset

---
+++ Segmentation/lib/dataset.py
@@ -0,0 +1,31 @@
+# https://www.kaggle.com/balraj98/cvcclinicdb
+
+class CVCClinicDB(torch.utils.data.Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        # Get the image & mask path
+        row = self.df.iloc[idx]
+        
+        # Open the files        
+        image = cv2.cvtColor(cv2.imread(row['image']), cv2.COLOR_BGR2RGB) / 255.
+        mask = self.encode_mask(cv2.imread(row['mask'], cv2.IMREAD_GRAYSCALE) / 255.)
+        
+        # Transformation
+        if self.transform:
+            transformed = self.transform(image=image, mask=mask)
+            image = transformed['image']
+            mask = transformed['mask']
+        
+        return image, mask
+    
+    def encode_mask(self, mask):
+        mask[mask >= 0.5] = 1.
+        mask[mask < 0.5] = 0.
+        
+        return mask
